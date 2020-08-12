c10e4ad08d36c9a43a9aa44a5bd194576da49d19
Putra <raihanputraramd@gmail.com>
2020-08-12 11:55:31 +0700

    add Modal

--- app/src/main/java/com/example/iwuapk/layout/TambahDosenActivity.java
+++ app/src/main/java/com/example/iwuapk/layout/TambahDosenActivity.java
@@ -1,5 +1,6 @@
 package com.example.iwuapk.layout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.example.iwuapk.R;
@@ -8,13 +9,17 @@ import com.example.iwuapk.model.Dosen;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -43,10 +48,10 @@ public class TambahDosenActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showDialogForm();
             }
         });
+
     }
 
     private ArrayList<Dosen> showDataDosen(){
@@ -58,5 +63,15 @@ public class TambahDosenActivity extends AppCompatActivity {
             list.add(dosen);
         }
         return list;
+    }
+
+    private void showDialogForm(){
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(TambahDosenActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.form_add_dosen, null);
+        dialog.setView(dialogView);
+        dialog.setCancelable(true);
+
+        dialog.show();
     }
 }
--- app/src/main/java/com/example/iwuapk/layout/TambahMahasiswaActivity.java
+++ app/src/main/java/com/example/iwuapk/layout/TambahMahasiswaActivity.java
@@ -1,5 +1,6 @@
 package com.example.iwuapk.layout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.example.iwuapk.adapter.TambahMahasiswaAdapter;
@@ -7,12 +8,15 @@ import com.example.iwuapk.model.Mahasiswa;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.iwuapk.R;
 
@@ -47,8 +51,7 @@ public class TambahMahasiswaActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showDialogForm();
             }
         });
     }
@@ -64,5 +67,15 @@ public class TambahMahasiswaActivity extends AppCompatActivity {
             list.add(mahasiswa);
         }
         return list;
+    }
+
+    private void showDialogForm(){
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(TambahMahasiswaActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.form_add_mahasiswa, null);
+        dialog.setView(dialogView);
+        dialog.setCancelable(true);
+
+        dialog.show();
     }
 }
